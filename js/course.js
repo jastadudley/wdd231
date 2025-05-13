becc8fce4ef66cdad410e0726c192377892b312c
jastadudley <jastamdudley@gmail.com>
2025-05-13 11:57:06 -0700

    adding coarse array

---
+++ js/course.js
@@ -0,0 +1,44 @@
+const courses = [
+    {
+      code: "WDD 130",
+      name: "Web Fundamentals",
+      credits: 3,
+      completed: true,
+      category: "WDD"
+    },
+    {
+      code: "WDD 131",
+      name: "Dynamic Web Fundamentals",
+      credits: 3,
+      completed: false,
+      category: "WDD"
+    },
+    {
+      code: "CSE 110",
+      name: "Intro to Programming",
+      credits: 2,
+      completed: true,
+      category: "CSE"
+    },
+    {
+      code: "CSE 111",
+      name: "Programming with Functions",
+      credits: 2,
+      completed: true,
+      category: "CSE"
+    },
+    {
+      code: "CSE 210",
+      name: "Programming with Classes",
+      credits: 3,
+      completed: false,
+      category: "CSE"
+    },
+    {
+      code: "WDD 231",
+      name: "Front-end Web Development I",
+      credits: 3,
+      completed: false,
+      category: "WDD"
+    }
+  ];
