13d2c015c827ebaccdf0b6d165742c360619c552
jastadudley <jastamdudley@gmail.com>
2025-05-13 10:05:59 -0700

    Added main and JS.

---
+++ js/date.js
@@ -0,0 +1,8 @@
+// date.js
+
+const yearSpan = document.querySelector('#year');
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastModified = document.querySelector('#lastModified');
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
