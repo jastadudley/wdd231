15431d11fc9f140891a34b7c78f45d0afcd9a9b0
jastadudley <jastamdudley@gmail.com>
2025-06-07 11:12:48 -0700

    Creating thankyou.html next.

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -3,5 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const timestampField = document.getElementById('timestamp');
     const now = new Date().toISOString();
     timestampField.value = now;
-  });
+});
+
+
+
+//Model
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".join-form");
+    const modal = document.getElementById("confirmationModal");
+    const closeModalBtn = document.getElementById("closeModal");
+    const timestampField = document.getElementById("timestamp");
+  
+    timestampField.value = new Date().toISOString();
+  
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+  
+      modal.showModal();
+    });
+  
+    closeModalBtn.addEventListener("click", () => {
+      modal.close();
+      form.reset(); 
+    });
+});
   
